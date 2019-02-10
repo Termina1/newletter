150b488479c09883a7c748c9928f68b47892d8ee
Termina1 <terminal2010@gmail.com>
2019-02-11 00:46:07 +0300

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,6 @@
 #/bin/bash
-docker save newsletter | gzip | ssh core@$PROD_IP "cat | gzip -d -c | docker load && (docker stop newsletter || true); docker rm newsletter || true; docker run -d --restart=always --name newsletter newsletter"
+docker build -t newsletter-base .
+stack image container
+docker save newsletter | gzip | \
+  ssh core@$PROD_IP \
+  "cat | gzip -d -c | docker load && (docker stop newsletter || true); docker rm newsletter || true; docker run -d --restart=always --name newsletter newsletter"
